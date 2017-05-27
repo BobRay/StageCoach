ad1de8196b813c24fa3c7b7e98fb7d404a0c29de
bobray <bobray@softville.com>
2017-05-27 00:44:21 -0500

    Update copyrights

--- _build/resolvers/tv.resolver.php
+++ _build/resolvers/tv.resolver.php
@@ -2,7 +2,7 @@
 /**
 * Resolver to connect TVs to templates for StageCoach extra
 *
-* Copyright 2012-2014 by Bob Ray <http://bobsguides.com>
+* Copyright 2012-2017 by Bob Ray <http://bobsguides.com>
 * Created on 03-05-2013
 *
  * StageCoach is free software; you can redistribute it and/or modify it under the
